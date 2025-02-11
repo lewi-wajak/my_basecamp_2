bc3a97e97cdec80cb035274787e6debdfe97b101
lewiwajak <lewiwajak@gmail.com>
2025-02-11 09:53:00 +0100

    messages set up was a success

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,61 +1,66 @@
 class MessagesController < ApplicationController
-    before_action :set_project_and_thread
-    before_action :set_message, only: [:edit, :update, :destroy]
-    before_action :check_project_membership, only: [:create, :edit, :update, :destroy]
-    before_action :authorize_user, only: [:edit, :update, :destroy]
-  
-    def create
-      @message = @project_thread.messages.new(message_params)
-      @message.user = current_user
-  
-      if @message.save
-        redirect_to project_project_thread_path(@project, @project_thread), notice: "Message sent successfully!"
-      else
-        redirect_to project_project_thread_path(@project, @project_thread), alert: "Failed to send message."
-      end
+  before_action :set_project_and_thread
+  before_action :set_message, only: [:edit, :update, :destroy]
+  before_action :authorize_project_member, only: [:create, :edit, :update, :destroy]
+  before_action :authorize_user, only: [:edit, :update, :destroy]
+
+  def create
+    @message = @project_thread.messages.new(message_params)
+    @message.user = current_user
+
+    if @message.save
+      redirect_to project_project_thread_path(@project, @project_thread), notice: "Message sent successfully!"
+    else
+      redirect_to project_project_thread_path(@project, @project_thread), alert: "Failed to send message."
     end
-  
-    def edit
+  end
+
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to project_project_thread_path(@project, @project_thread), notice: "Message updated successfully!"
+    else
+      render :edit
     end
-  
-    def update
-      if @message.update(message_params)
-        redirect_to project_project_thread_path(@project, @project_thread), notice: "Message updated successfully!"
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
+  end
+
+  def destroy
+    @message = @project_thread.messages.find(params[:id])
+    
+    if @message.user == current_user || current_user.has_role?(:admin)
       @message.destroy
       redirect_to project_project_thread_path(@project, @project_thread), notice: "Message deleted successfully!"
-    end
-  
-    private
-  
-    def set_project_and_thread
-      @project = Project.find(params[:project_id])
-      @project_thread = @project.project_threads.find(params[:project_thread_id])
-    end
-  
-    def set_message
-      @message = @project_thread.messages.find(params[:id])
-    end
-  
-    def check_project_membership
-      unless @project.users.include?(current_user)
-        redirect_to project_project_thread_path(@project, @project_thread), alert: "You can't modify messages in this thread."
-      end
-    end
-  
-    def authorize_user
-      unless @message.user == current_user || current_user.has_role?(:admin)
-        redirect_to project_project_thread_path(@project, @project_thread), alert: "You can't edit or delete this message."
-      end
-    end
-  
-    def message_params
-      params.require(:message).permit(:content)
+    else
+      redirect_to project_project_thread_path(@project, @project_thread), alert: "You are not authorized to delete this message."
     end
   end
   
+  private
+
+  def set_project_and_thread
+    @project = Project.find(params[:project_id])
+    @project_thread = @project.project_threads.find(params[:project_thread_id])
+  end
+
+  def set_message
+    @message = @project_thread.messages.find(params[:id])
+  end
+
+  def authorize_user
+    unless @message.user == current_user || current_user.has_role?(:admin)
+      redirect_to project_project_thread_path(@project, @project_thread), alert: "You can't edit or delete this message."
+    end
+  end
+
+  def authorize_project_member
+    unless @project.users.include?(current_user) || @project.creator == current_user
+      redirect_to project_path(@project), alert: "You must be a project member to perform this action."
+    end
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
--- app/controllers/project_threads_controller.rb
+++ app/controllers/project_threads_controller.rb
@@ -1,38 +1,31 @@
 class ProjectThreadsController < ApplicationController
   before_action :set_project
   before_action :set_project_thread, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_admin, only: [:new, :create, :edit, :update, :destroy]
+
+  def index
+    @threads = @project.project_threads
+  end
+
+  def show
+  end
 
   def new
     @project_thread = @project.project_threads.new
   end
 
-
-  def index
-    @threads = @project.project_threads
-  end
-  
-
-  def edit
-    @project_thread = @project.project_threads.find(params[:id])
-  end
-  
-
   def create
-    unless current_user.has_role?(:admin)
-      redirect_to project_project_threads_path(@project), alert: "Only admins can create threads."
-      return
-    end
-  
     @project_thread = @project.project_threads.new(thread_params)
-    @project_thread.user = current_user
-  
+
     if @project_thread.save
       redirect_to project_project_thread_path(@project, @project_thread), notice: "Thread created successfully!"
     else
       render :new
     end
   end
-  
+
+  def edit
+  end
 
   def update
     if @project_thread.update(thread_params)
@@ -40,9 +33,6 @@ class ProjectThreadsController < ApplicationController
     else
       render :edit
     end
-  end
-
-  def show
   end
 
   def destroy
@@ -62,5 +52,11 @@ class ProjectThreadsController < ApplicationController
 
   def thread_params
     params.require(:project_thread).permit(:title)
+  end
+
+  def authorize_admin
+    unless current_user.has_role?(:admin)
+      redirect_to project_project_threads_path(@project), alert: "Only admins can manage threads."
+    end
   end
 end
